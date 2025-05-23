f4c6af7aed9e5c582ef3d78aadaa327ad3e2f686
Vaibhav Rokade <rokadevaibhav04@.com>
2025-05-23 01:27:20 -0400

    dfghjk

--- 24logic.c
+++ 24logic.c
@@ -1,28 +1,24 @@
 #include <stdio.h>
+#include <ctype.h>
 int main(int argc, char const *argv[])
 {
     char ch;
     printf("Enter a Charactor :");
     scanf("%c", &ch);
 
+    ch = tolower(ch);
     switch (ch)
     {
-    case 'A':
     case 'a':
-    case 'E':
     case 'e':
-    case 'I':
     case 'i':
-    case 'O':
     case 'o':
-    case 'U':
     case 'u':
         printf("Charactor is Vowel");
         break;
 
     default:
         printf("Charctor is Not Vowel");
-        break;
     }
     return 0;
 }
